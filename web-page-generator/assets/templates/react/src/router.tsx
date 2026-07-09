547a8d4183f33153b5d0cc3cb0bbd4018429a4ff
mario0723 <70555776+mario0723@users.noreply.github.com>
2026-07-09 12:44:01 +0800

    Delete web-page-generator directory

--- web-page-generator/assets/templates/react/src/router.tsx
+++
@@ -1,8 +0,0 @@
-import { RouterProvider, createHashRouter } from "react-router-dom";
-import { routeTree } from "./routeTree.gen";
-
-export const router = createHashRouter(routeTree);
-
-export function AppRouter() {
-  return <RouterProvider router={router} />;
-}
